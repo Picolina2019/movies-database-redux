0a9796be7f02750552bd1080a9fb60368582a472
 Katia S <katesakowicz@MacBook-Pro-4.local>
2021-03-31 15:59:20 +0200

    movie info

--- src/components/pages/MoviesPage/MoviePage.jsx
+++ src/components/pages/MoviesPage/MoviePage.jsx
@@ -1,15 +1,28 @@
-import React, { Component } from 'react';
+import { useEffect, useState } from 'react';
 import CallApi from '../../../api/api';
 
+const MoviePage = (props) => {
+  const [movie, setMovie] = useState({});
+  const [loading, setLoading] = useState(false);
 
-export default class MoviePage extends Component {
-  componentDidMount() {
-    CallApi.get(`/movie/${this.props.match.params.id}`).then((data) => {
-      
-    });
-  }
+  useEffect(() => {
+    setLoading(true);
+    CallApi.get(`/movie/${props.match.params.id}`).then((data) =>
+      setMovie(data.belongs_to_collectionc)
+    );
+    setLoading(false);
+  }, [props.match.params.id]);
+  console.log(movie);
+  const newMovie = Object.entries(movie).map(([key, value]) => {
+    return (
+      <li key={key.id}>
+        <span>
+          {key}:{value}
+        </span>
+      </li>
+    );
+  });
 
-  render() {
-    return <div>Movie</div>;
-  }
-}
+  return <div>{newMovie}</div>;
+};
+export default MoviePage;
